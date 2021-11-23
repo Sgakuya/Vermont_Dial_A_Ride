b06a8c5d9f9f35403e2b156655a4126a1abb4d46
Luke Lorenz <lukelorenz@lukes-air-2.middlebury.edu>
2021-11-23 09:46:11 -0500

    Fixed map display issue + barstyle

--- website/forms.py
+++ website/forms.py
@@ -6,8 +6,8 @@ from .models import Ride
 class UserForm(forms.ModelForm):
     origin = forms.CharField(widget=forms.TextInput(attrs={'id':'from', 'placeholder':'origin', 'class':'form-control'}))
     destination = forms.CharField(widget=forms.TextInput(attrs={'id':'to', 'placeholder':'destination', 'class':'form-control'}))
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Name ', 'class':'form-control'}))
-    time = forms.DateTimeField(widget=forms.TimeInput(attrs={'placeholder':'Input Time like: 2021-11-25 14:30:59', 'class':'form-control'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'id': 'requestMethod', 'placeholder':'Name ', 'class':'form-control'}))
+    time = forms.DateTimeField(widget=forms.TimeInput(attrs={'id': 'pickDate','placeholder':'Input Time like: 2021-11-25 14:30:59', 'class':'form-control'}))
 
     class Meta():
         model = Ride
